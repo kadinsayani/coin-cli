b67f8b3d82522e4a544ba919ef51b9099c49fd9d
Kadin Sayani <kadinsayani8@gmail.com>
2022-10-23 20:37:24 -0600

    successful api request

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use reqwest::Response;
 use std::error::Error;
 
 #[derive(Parser)]
@@ -7,27 +6,29 @@ struct Cli {
     command: String,
 }
 
-struct Coin {
-    id: String,
-    symbol: String,
-    name: String,
-}
+// struct Coin {
+//     id: String,
+//     symbol: String,
+//     name: String,
+// }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Cli::parse();
     let command = args.command;
     if command == "list" {
         let response = reqwest::blocking::get("https://api.coingecko.com/api/v3/coins/list")?;
-        let coins = parse_coins(response)?;
-        for coin in coins {
-            println!("{} - {}", coin.id, coin.name);
-        }
+        println!("{}", response.status());
+        //     let coins = parse_coins(response)?;
+        //     for coin in coins {
+        //         println!("{} - {}", coin.id, coin.name);
+        //     }
+        // }
     }
     Ok(())
 }
 
-fn parse_coins(response: Response) -> Result<Vec<Coin>, Box<dyn Error>> {
-    let body = response.text()?;
-    let coins: Vec<Coin> = serde_json::from_str(&body)?;
-    Ok(coins)
-}
+// fn parse_coins(response: Response) -> Result<Vec<Coin>, Box<dyn Error>> {
+//     let body = response.text()?;
+//     let coins: Vec<Coin> = serde_json::from_str(&body)?;
+//     Ok(coins)
+// }
