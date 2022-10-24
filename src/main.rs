29f699e88a8255bdddee08aa3c8bc3636aed62f3
Kadin Sayani <kadinsayani8@gmail.com>
2022-10-23 22:16:18 -0600

    btc price request complete

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,5 @@
-use clap::Parser;
-use std::error::Error;
+mod crypto_price;
 
-#[derive(Parser)]
-struct Cli {
-    command: String,
+fn main() {
+    crypto_price::crypto_price().unwrap();
 }
-
-// struct Coin {
-//     id: String,
-//     symbol: String,
-//     name: String,
-// }
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let args = Cli::parse();
-    let command = args.command;
-    if command == "list" {
-        let response = reqwest::blocking::get("https://api.coingecko.com/api/v3/coins/list")?;
-        println!("{}", response.status());
-        //     let coins = parse_coins(response)?;
-        //     for coin in coins {
-        //         println!("{} - {}", coin.id, coin.name);
-        //     }
-        // }
-    }
-    Ok(())
-}
-
-// fn parse_coins(response: Response) -> Result<Vec<Coin>, Box<dyn Error>> {
-//     let body = response.text()?;
-//     let coins: Vec<Coin> = serde_json::from_str(&body)?;
-//     Ok(coins)
-// }
